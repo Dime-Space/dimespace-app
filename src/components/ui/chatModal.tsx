0d7308093b2c97f8ac9bbf0a09b5f7a9debcdecc
craco <gctasars@viptech.com.br>
2025-06-24 07:16:33 -0300

    feat: integrate chat with back-end

--- src/components/ui/chatModal.tsx
+++ src/components/ui/chatModal.tsx
@@ -3,6 +3,8 @@ import { Sheet, SheetContent } from '@/components/ui/sheet';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useForm } from 'react-hook-form';
+import { useQuery } from '@tanstack/react-query';
+import { Chat, getChatHistory, listChats } from '@/services/chat/chatService';
 
 interface ChatUser {
   id: number;
@@ -14,8 +16,6 @@ interface ChatUser {
 interface ChatModalProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
-  messages: { id: number; text: string; sender: 'user' | 'bot' }[];
-  onSendMessage: (message: string) => void;
 }
 
 const testUsers: ChatUser[] = [
@@ -36,31 +36,47 @@ const testUsers: ChatUser[] = [
     nome: 'Nvidia',
     avatar: '/src/assets/images/nvidia.jpg',
     lastMessage: 'bem vindo a nvidia  ...',
-  }
+  },
 ];
 
 type ChatFormValues = {
   message: string;
 };
 
-const ChatModal: React.FC<ChatModalProps> = ({
-  open,
-  onOpenChange,
-  messages,
-  onSendMessage,
-}) => {
-  const [selectedChat, setSelectedChat] = useState<ChatUser | null>(null);
+const ChatModal: React.FC<ChatModalProps> = ({ open, onOpenChange }) => {
+  const [selectedChat, setSelectedChat] = useState<Chat | null>(null);
   const [search, setSearch] = useState('');
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
+  const { data: chats } = useQuery({
+    queryKey: ['chats'],
+    queryFn: async () => {
+      const response = await listChats();
+      return response.data;
+    },
+  });
+
+  const { data: chatHistory } = useQuery({
+    queryKey: ['chat-history'],
+    enabled: !!selectedChat,
+    queryFn: async () => {
+      if (!selectedChat) return undefined;
+      const response = await getChatHistory(selectedChat?.id);
+      return response.data;
+    },
+  });
+
   const { register, handleSubmit, reset } = useForm<ChatFormValues>();
+
+  const messages = [];
+  const onSendMessage = (message: string) => {};
 
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   }, [messages, open, selectedChat]);
 
   const filteredUsers = testUsers.filter((user) =>
-    user.nome.toLowerCase().includes(search.toLowerCase())
+    user.nome.toLowerCase().includes(search.toLowerCase()),
   );
 
   const handleSend = (data: ChatFormValues) => {
@@ -83,7 +99,7 @@ const ChatModal: React.FC<ChatModalProps> = ({
         className="flex flex-col h-full p-0 max-w-md w-full bg-white"
       >
         {!selectedChat ? (
-          <div className="flex flex-col h-full">
+          <div className="flex flex-col h-full ">
             <div className="p-4 border-b border-[#eee] bg-[#f9f9f9]">
               <Input
                 placeholder="Pesquisar"
@@ -93,26 +109,32 @@ const ChatModal: React.FC<ChatModalProps> = ({
               />
             </div>
             <div className="flex-1 overflow-y-auto">
-              {filteredUsers.map((user, idx) => (
+              {chats?.map((chat, idx) => (
                 <button
-                  key={user.id}
-                  className={`flex items-center w-full px-4 py-3 transition-colors border-b border-[#eee] ${
+                  key={chat.id}
+                  className={`flex items-center w-full px-4 py-3 transition-colors border-b border-[#eee] cursor-pointer ${
                     idx === 1 ? 'bg-[#f3f3f3]' : 'bg-white'
                   } hover:bg-[#f3f3f3]`}
-                  onClick={() => setSelectedChat(user)}
+                  onClick={() => setSelectedChat(chat)}
                   style={{ outline: 'none' }}
                 >
                   <img
-                    src={user.avatar}
-                    alt={user.nome}
-                    className="w-12 h-12 rounded-full object-cover mr-4"
+                    // src={chat.avatar}
+                    // alt={chat.nome}
+                    className="w-12 h-12 rounded-full object-cover mr-4 bg-gray-200"
                   />
                   <div className="flex-1 text-left">
                     <div className="flex items-center justify-between">
-                      <span className="font-semibold text-black text-base">{user.nome}</span>
+                      <span className="font-semibold text-black text-base">
+                        {chat.company.name}
+                      </span>
                     </div>
                     <span className="text-[#666] text-sm truncate block max-w-[180px]">
-                      {user.lastMessage}
+                      {new Date(chat.created_at).toLocaleDateString('pt-BR')}{' '}
+                      {new Date(chat.created_at).toLocaleTimeString('pt-BR', {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
                     </span>
                   </div>
                 </button>
@@ -131,25 +153,27 @@ const ChatModal: React.FC<ChatModalProps> = ({
                 <span className="text-2xl text-black">{'←'}</span>
               </Button>
               <img
-                src={selectedChat.avatar}
-                alt={selectedChat.nome}
-                className="w-12 h-12 rounded-full object-cover"
+                // src={selectedChat.avatar}
+                // alt={selectedChat.nome}
+                className="w-12 h-12 rounded-full object-cover bg-gray-200"
               />
               <div>
-                <div className="font-semibold text-black">{selectedChat.nome}</div>
+                <div className="font-semibold text-black">
+                  {selectedChat.name}
+                </div>
               </div>
             </div>
             <div className="flex-1 overflow-y-auto px-4 py-2 flex flex-col bg-white">
-              {messages.map((msg) => (
+              {chatHistory?.messages.map((msg) => (
                 <div
                   key={msg.id}
                   className={`p-2 rounded-lg max-w-xs mb-2 ${
-                    msg.sender === 'user'
+                    msg.user
                       ? 'bg-blue-500 text-white self-end ml-auto'
                       : 'bg-gray-200 text-gray-900 self-start mr-auto'
                   }`}
                 >
-                  {msg.text}
+                  {msg.content}
                 </div>
               ))}
               <div ref={messagesEndRef} />
